2b57307235e4907d6369cec433f5babf49993da5
Daniel Henseler <danielhenseler@badcodes.de>
2017-01-20 22:09:30 +0100

    Added basic level generation and wave generation

---
+++ Assets/MapCube.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour {
+	public Transform ProbePosition;
+
+	private void Start () {
+		ProbePosition = GetComponentInChildren<Transform>();
+	}
+}
